2607e63f261f8fcbdfd4a48f489bbbbe6e9f563e
Herms <johnlawrencehermo@gmail.com>
2023-07-27 16:03:36 +0800

    init memegenerator

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Nav from "./components/Nav/Nav";
+import Meme from "./components/Meme/Meme";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Nav />
+        <Meme />
+      </div>
+    </>
+  );
+}
+
+export default App;
