741f5bab50c84ac55ddb3e02926dde1e66b83de7
devxyn <johnlawrencehermo@gmail.com>
2025-11-03 18:07:23 +0800

    feat: migrate App component from JavaScript to TypeScript and update component structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import MemeSection from "./components/MemeSection";
+
+const App = () => {
+  return (
+    <main className='min-h-screen'>
+      <Navbar />
+      <MemeSection />
+    </main>
+  );
+};
+
+export default App;
